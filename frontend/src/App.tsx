df135fb3ed88fb48477928c4fca2794125c062ec
nandini <nandninipaidesetty@gmail.com>
2025-04-03 16:35:10 -0500

    added course insert frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,24 @@
-import { Link, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import ProfilePage from './pages/ProfilePage'
+import { Link, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import GradeInputPage from './pages/GradeInputPage';
 
 function App() {
-
   return (
     <div>
       <nav>
         <Link to='/'>Home</Link>
         <Link to='/profile'>Profile</Link>
+        <Link to='/inputGrade'>Input</Link>
       </nav>
 
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/inputGrade' element={<GradeInputPage />} /> {/* <-- Update this line */}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
