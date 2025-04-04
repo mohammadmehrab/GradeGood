d9b36736bebb896923909b6c2f05245531cfe64f
Nguyen (Tony) Do <69228834+bigcouchman@users.noreply.github.com>
2025-04-03 22:56:01 -0500

    Added link and route to GPAcalc.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Link, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
 import GradeInputPage from './pages/GradeInputPage';
+import GPAcalc from './pages/GPAcalc';
 
 function App() {
   return (
@@ -10,12 +11,14 @@ function App() {
         <Link to='/'>Home</Link>
         <Link to='/profile'>Profile</Link>
         <Link to='/inputGrade'>Input</Link>
+        <Link to='/gpacalc'>GPA Calculator  </Link>
       </nav>
 
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/profile' element={<ProfilePage />} />
         <Route path='/inputGrade' element={<GradeInputPage />} /> {/* <-- Update this line */}
+        <Route path='/gpacalc' element={<GPAcalc/>}/>
       </Routes>
     </div>
   );
