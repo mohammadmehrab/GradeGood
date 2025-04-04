8ded47828792639d8186da9a569bd7b3ce387ee0
nxautd <nxa220118@utdallas.edu>
2025-04-04 16:03:57 -0500

    Frontend for login/logout. Added navigation bar.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,54 @@
-import { Link, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import GradeInputPage from './pages/GradeInputPage';
-import GPAcalc from './pages/GPAcalc';
+import { Link, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import ForgotPassword from "./components/forgotpassword";
+import PrivateRoute from "./components/privateroute";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import GradeInputPage from "./pages/GradeInputPage";
+import GPAcalc from "./pages/GPAcalc";
 
 function App() {
   return (
     <div>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/profile'>Profile</Link>
-        <Link to='/inputGrade'>Input</Link>
-        <Link to='/gpacalc'>GPA Calculator  </Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4 shadow-sm">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Home</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/profile">Profile</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/inputGrade">Input Grade</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/gpacalc">GPA Calculator</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">Dashboard</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">Signup</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/inputGrade' element={<GradeInputPage />} /> {/* <-- Update this line */}
-        <Route path='/gpacalc' element={<GPAcalc/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+        <Route path="/inputGrade" element={<PrivateRoute><GradeInputPage /></PrivateRoute>} />
+        <Route path="/gpacalc" element={<PrivateRoute><GPAcalc /></PrivateRoute>} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </div>
   );
