231a613d45f432f37e2b7b3d8b2e6ebd6453ad3a
mohammadmehrab <108484416+mohammadmehrab@users.noreply.github.com>
2025-04-14 21:43:56 -0500

    Implemented profile page frontend and improved app layout

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,26 +1,27 @@
-import styles from './HomePage.module.css'
+import styles from "./HomePage.module.css";
 
 export default function HomePage() {
-
-    return (
-        <div>
-            <h1 className={styles.title}>Welcome To GradeGood</h1>
-            <h2 className={styles.subtitle}>Change your study game today by creating an account.</h2>
-            {/* <form className={`${styles.loginForm} ${styles.testBorder}`}>
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome To GradeGood</h1>
+      <h2 className={styles.subtitle}>
+        Change your study game today by creating an account.
+      </h2>
+      {/* <form className={`${styles.loginForm} ${styles.testBorder}`}>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="username-input">Username:</label>
-                    <input id="username-input" />
+                    <label htmlFor="username-input" className='label'>Username:</label>
+                    <input id="username-input" className='input' />
                 </div>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="password-input">Password:</label>
-                    <input id="password-input" type="password" />
+                    <label htmlFor="password-input" className='label'>Password:</label>
+                    <input id="password-input" type="password"  className='input'/>
                 </div>
                 <div className={styles.inputGroup}>
-                    <label htmlFor="confirm-password-input">Confirm Password:</label>
-                    <input id="confirm-password-input" type="password" />
+                    <label htmlFor="confirm-password-input" className='label'>Confirm Password:</label>
+                    <input id="confirm-password-input" type="password" className='input'/>
                 </div>
                 <button className={styles.createAccountBtn} onClick={submit}>Create Account</button>
             </form> */}
-        </div>
-    )
+    </div>
+  );
 }
