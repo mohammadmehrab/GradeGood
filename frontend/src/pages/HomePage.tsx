3b307d2c359ceaa359771cbdcad32da8f8b00bf4
nazin <nazin.nazeri@gmail.com>
2025-04-16 20:19:43 -0500

    added about us

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,12 +2,42 @@ import styles from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
+    
     <div className="h-full flex flex-col justify-center">
-      <h1 className={styles.title}>Welcome To GradeGood</h1>
-      <h2 className={styles.subtitle}>
-        Change your study game today by creating an account.
-      </h2>
-      {/* <form className={`${styles.loginForm} ${styles.testBorder}`}>
+      <div className= "flex h-screen">
+        <div className= "w-1/2 flex items-center">
+      <img src="/homepagepic.png"
+                    alt="homepic"
+                    className="w-200 h-100 mr-5"/>
+       </div>
+       <div className="w-1/2 flex flex-col space-y-4 ml-10 mr-10">
+      <div className="mt-8">
+      <h1 className={styles.title }>Welcome To GradeGood</h1>
+      </div>
+      <h1>
+      About Creators:
+      </h1>
+      <h1>
+      We are aspiring software developers who  aim to  create an interactive and useful aid for students of all levels. GradeGood is not a game, it is an impactful medium for academic success.
+      </h1>
+      <h1>
+      What is GradeGood?
+      </h1>
+      <h1>
+  GradeGood performs GPA calculation and organizes your study calendar, to ensure you are on the right track for a bright future.
+      </h1>
+       </div>
+      </div>
+
+      </div>
+      
+    
+    
+    
+  );
+}
+
+  {/* <form className={`${styles.loginForm} ${styles.testBorder}`}>
                 <div className={styles.inputGroup}>
                     <label htmlFor="username-input" className='label'>Username:</label>
                     <input id="username-input" className='input' />
@@ -20,8 +50,5 @@ export default function HomePage() {
                     <label htmlFor="confirm-password-input" className='label'>Confirm Password:</label>
                     <input id="confirm-password-input" type="password" className='input'/>
                 </div>
-                <button className={styles.createAccountBtn} onClick={submit}>Create Account</button>
+                <button className={styl\es.createAccountBtn} onClick={submit}>Create Account</button>
             </form> */}
-    </div>
-  );
-}
