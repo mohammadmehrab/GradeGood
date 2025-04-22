fcea36c11bb86e751ebcfcdd2122292d78817eb4
bigcouchman <dophucnguyen2005@gmail.com>
2025-04-22 15:48:33 -0500

    Made goal setting feature

---
+++ frontend/src/pages/GoalSet.tsx
@@ -0,0 +1,328 @@
+import { useState } from "react";
+import styles from "./GoalSet.module.css";
+import ProgChart from "./ProgChart";
+
+type Policy = {
+  name: string;
+  goal: number;
+  weightage: number;
+  progress: number[];
+};
+
+export default function GoalSet(){
+    const [courseID, setCourseID] = useState("");
+    const [courseName, setCourseName] = useState("");
+    const [msg, setMsg] = useState("");
+    const [displayResult, setDisplayResult] = useState(false);
+    const [policy, setPolicy] = useState<Policy[]>([
+        {name: "Homework", goal: 100, weightage: 0, progress: []},
+        {name: "Quiz", goal: 100, weightage: 0, progress: []},
+    ]);
+
+    const [progInputs, setProgInputs] = useState<number[]>(
+        policy.map(()=>0)
+    )
+
+    const newGrade = (i: number, grade: number)=>{
+        if (grade < 0 || grade > 100 || isNaN(grade)){
+            setMsg("Grade must be between 0 to 100");
+            return;
+        }
+        const upGrade = [...policy];
+        const upInputs = [...progInputs];
+        upGrade[i].progress.push(grade);
+        upInputs[i] = 0;
+        setPolicy(upGrade);
+        setProgInputs(upInputs);
+        
+    };
+
+    const removeGrade = (polI: number, gI: number) => {
+        const upInputs = [...policy];
+        upInputs[polI].progress.splice(gI, 1);
+        setPolicy(upInputs);
+    }
+
+    const newWeight = (i: number, weight: number) => {
+        const upWeight = [...policy];
+        upWeight[i].weightage = weight;
+        setPolicy(upWeight);
+    }
+
+    const findAvg = (grades: number[]) =>{
+        if (grades.length === 0){
+            return 0;
+        }
+        const finalGrade = grades.reduce((total,g)=> total + g, 0);
+        return finalGrade/grades.length;
+    };
+
+    const getFinalGrade = () => {
+        let totalGrade = 0;
+        policy.forEach((p) => {
+            const avg = findAvg(p.progress);
+            totalGrade += (avg * p.weightage) / 100;
+        });
+        return getGPA(totalGrade);
+    }
+
+    const getGPA = (gradeFin: number) => {
+        if (gradeFin >= 93) {
+            return 4.0;
+          } else if (gradeFin >= 90) {
+            return 3.67;
+          } else if (gradeFin >= 87) {
+            return 3.33;
+          } else if (gradeFin >= 83) {
+            return 3.0;
+          } else if (gradeFin >= 80) {
+            return 2.67;
+          } else if (gradeFin >= 77) {
+            return 2.33;
+          } else if (gradeFin >= 73) {
+            return 2.0;
+          } else if (gradeFin >= 70) {
+            return 1.67;
+          } else if (gradeFin >= 67) {
+            return 1.33;
+          } else if (gradeFin >= 63) {
+            return 1.00;
+          } else if (gradeFin >= 60) {
+            return 0.67;
+          } else if (gradeFin >= 57) {
+            return 0.33;
+          } else {
+            return 0.0;
+          }
+          
+    }
+
+    const checkInputs = () => {
+        // If any of the fields are blank
+        if (
+          courseID.trim() === "" ||
+          courseName.trim() === "" ||
+          policy.some((pol) => pol.name.trim() === "")
+        ) {
+          setMsg("Error: You must have left some fields empty!");
+          setDisplayResult(true);
+          return false;
+        }
+        // If policy length is not in range
+        if (policy.some((p) => p.name.length < 1 || p.name.length > 15)) {
+          setMsg("Error: Your policy must be in length between 1 to 15.");
+          setDisplayResult(true);
+          return false;
+        }
+    
+        // If courseID is not a 4 digit integer
+        if (!/^\d{4}$/.test(courseID)) {
+          setMsg("Error: Your course ID must be a 4 digit integer!");
+          setDisplayResult(true);
+          return false;
+        }
+        // if course name length is not in range
+        if (courseName.length < 1 || courseName.length > 15) {
+          setMsg("Error: Your course name must be in length between 1 to 15.");
+          setDisplayResult(true);
+          return false;
+        }
+    
+        //Add up the weightage of every policy, check if they add up to 100
+        const weight = policy.reduce((sum, pol) => sum + Number(pol.weightage), 0);
+        if (weight !== 100) {
+          setMsg("Error: Total weightage needs to add up to 100. ");
+          setDisplayResult(true);
+          return false;
+        }
+    
+        //check every policy's grade and weightage to see if they are in range
+        for (const p of policy) {
+          if (p.weightage < 0 || p.weightage > 100) {
+            setMsg(
+              "Error: Weightage must be in range of 0 to 100. Also input must solely be integer..."
+            );
+            setDisplayResult(true);
+            return false;
+          }
+          if (p.progress.some((g) => g < 0 || g > 100)) {
+            setMsg(
+              "Error: Grade must be in range of 0 to 100. Also input must solely be integer..."
+            );
+            setDisplayResult(true);
+            return false;
+          }
+        }
+    
+        // return true if every check is passed
+        return true;
+      };
+
+    const handleSubmit = () =>{
+        if (checkInputs()){
+            const gpa = getFinalGrade();
+            setMsg(`Your GPA for the course: ${gpa.toFixed(2)}`);
+            setDisplayResult(true);
+        }
+    };
+
+    const feedBack = (goal: number, average: number)=>{
+        if (average >= goal){
+            return "That's what's up my goat! Keep up the great work!";
+        }
+        else if ((goal - average) <= 5){
+            return "You are so close! Get to the finish line!";
+        }
+        return "You still gotta push, but I believe in you!"
+    };
+
+    const newGoal = (i: number, g: number)=>{
+        const upGoal = [...policy];
+        upGoal[i].goal = g;
+        setPolicy(upGoal);
+    }
+
+    const newPolicyName = (i: number, pn: string)=>{
+        const upPolicy = [...policy];
+        upPolicy[i].name = pn;
+        setPolicy(upPolicy);
+    }
+
+    const newPolicy = () =>{
+        if (policy.length < 5){
+            setPolicy([...policy, {name: `Policy ${policy.length + 1}`, goal: 100, weightage: 0, progress: []}])
+        }
+    }
+
+    const removePolicy =()=>{
+        if (policy.length > 1){
+            setPolicy(policy.slice(0,-1));
+        }
+    }
+
+    return(
+        <div className={styles.regContain}>
+            <h1 className={styles.heading}>Set Your Grade Goal</h1>
+            <div className={styles.course_info}>
+                <div className={styles.input_contain}>
+                <label>
+                    Course ID:
+                </label>
+                <input
+                    type="text" value={courseID} onChange={(e) => setCourseID(e.target.value)}
+                    className={styles.input_box_policy}
+                />
+                </div>
+
+                <div className={styles.input_contain}>
+                <label>
+                    Course Name:
+                </label>
+                <input
+                    type="text" value={courseName} onChange={(e) => setCourseName(e.target.value)}
+                    className={styles.input_box_policy}
+                />
+                </div>
+            </div>
+
+            {policy.map((pol,i)=>{
+                const avg = findAvg(pol.progress);
+                return(
+                    <div key={i} className={styles.policy_area}>
+                        <h2>
+                            <input
+                                value={pol.name} type="text" onChange={(e) => newPolicyName(i,e.target.value)}
+                                className={styles.input_box_policy}
+                            />
+                        </h2>
+                        <div className={styles.input_contain}>
+                            <label>Set your goal: </label>
+                            <input 
+                                type="number" min={0} max={100} 
+                                value={pol.goal} onChange={(e) => newGoal(i, parseFloat(e.target.value))}
+                                className={styles.input_box_grade}
+                                />
+                            <div>
+                                <strong>Goal Grade: </strong>{pol.goal}
+                            </div>
+                        </div>
+
+                        <div className={styles.input_contain}>
+                            <label>Add Weightage: </label>
+                            <input
+                                type="number"
+                                min={0}
+                                max={100}
+                                value={pol.weightage}
+                                onChange={(e) => newWeight(i, parseFloat(e.target.value))}
+                                className={styles.input_box_grade}
+                            />
+                        </div>
+
+                        <div className={styles.input_contain}>
+                            <label>Add Progress Grade: </label>
+                            <input
+                                type="number"
+                                min={0}
+                                max={100}
+                                onChange={(e) => {
+                                    const val = parseFloat(e.target.value);
+                                    const updateInputs = [...progInputs];
+                                    updateInputs[i] = val;
+                                    setProgInputs(updateInputs);
+                                }}
+                                className={styles.input_box_grade}
+                            />
+                            <button onClick={()=> newGrade(i, progInputs[i])} className={styles.button_small}>
+                                Add Grade
+                            </button>
+                        </div>
+
+                        <div className={styles.input_contain}>
+                            <strong>Progress Entry:</strong> {pol.progress.length === 0 ? (
+                                <span>No element in progress</span>
+                            ):(
+                                <ul className={styles.gradeList}>
+                                    {pol.progress.map((grade, gIndex)=>(
+                                        <li key={gIndex}>
+                                            Grade {gIndex +1}: {grade}{" "}
+                                            <button onClick={()=>removeGrade(i,gIndex)} className={styles.button_small}>
+                                                Remove
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <p className={styles.average}>Current Average: {avg.toFixed(2)}</p>
+                        <p className={styles.feedback}>{feedBack(pol.goal, avg)}</p>     
+                    </div>
+                );
+            })}
+
+            <div className={styles.button_contain}>
+                <button onClick={newPolicy} disabled={policy.length >= 5} className={styles.button_reg}>
+                    Add Policy
+                </button>
+                <button onClick={removePolicy} disabled={policy.length <= 1} className={styles.button_reg}>
+                    Remove Policy
+                </button>
+                <button onClick={handleSubmit} className={styles.button_reg}>
+                    Find GPA and Chart
+                </button>
+            </div>
+
+            {displayResult &&
+                <div className={styles.result}>
+                    {msg}
+                </div>
+            }
+            {policy.some(p=>p.progress.length > 0) && (
+                <div className={styles.chart_contain}>
+                    <h3>Policy Progress Chart: {courseName.trim() && courseID.trim() ? `${courseName} (${courseID})` : "Nameless Course"}</h3>
+                    <ProgChart pols={policy}/>
+                </div>
+            )}
+        </div>
+    )
+}
--- frontend/src/pages/GradeInputPage.tsx
+++ frontend/src/pages/GradeInputPage.tsx
@@ -7,19 +7,22 @@ export default function GradeInputPage() {
   const [courseName, setCourseName] = useState("");
   const [creditHours, setCreditHours] = useState(0);
   const [grade, setGrade] = useState(0);
+  const [policy, setPolicy] = useState([{ name: "", weightage: 0, grade: 0 }]);
+  const [schedule, setSchedule] = useState([{day: "", startTime:"", endTime:""}])
 
   const { currentUser } = useAuth();
 
   const submit = async () => {
     // You can handle form submission logic here (e.g., state, validation, etc.)
-    console.log(courseName, creditHours, grade);
+    console.log(courseName, creditHours, grade,policy,schedule);
     if (currentUser && currentUser.email) {
       const returnMessage = await submitCourse(
         currentUser.email,
         courseName,
         creditHours,
-        grade
+        grade,
       );
+
       console.log(returnMessage);
     } else {
       console.log("user doesnt exist");
@@ -29,6 +32,46 @@ export default function GradeInputPage() {
     setCreditHours(0);
     setGrade(0);
   };
+
+    // Add new blank fields for a policy
+    const newPolicy = () => {
+      if (policy.length < 5) {
+        setPolicy([...policy, { name: "", weightage: 0, grade: 0 }]);
+      }
+    };
+  
+    // Remove fields of a policy
+    const removePolicy = () => {
+      if (policy.length > 1) {
+        setPolicy(policy.slice(0, -1));
+      }
+    };
+
+    const newSched = () => {
+      if (schedule.length < 5) {
+        setSchedule([...schedule, { day: "", startTime: "", endTime: "" }]);
+      }
+    };
+  
+    // Remove fields of a policy
+    const removeSched = () => {
+      if (schedule.length > 1) {
+        setSchedule(schedule.slice(0, -1));
+      }
+    };
+
+    const handleChange = (i: number, field: string, value: number | string) => {
+      const pol = [...policy];
+      pol[i] = { ...pol[i], [field]: value };
+      setPolicy(pol);
+    };
+
+    const handleSchedChange =(i: number, field: keyof (typeof schedule)[0], val: string)=>{
+      const createSchedule = [...schedule];
+      createSchedule[i][field] = val;
+      setSchedule(createSchedule);
+    }
+
 
   return (
     <div className="h-full">
@@ -60,22 +103,104 @@ export default function GradeInputPage() {
             onChange={(event) => setCreditHours(parseFloat(event.target.value))}
           />
         </div>
-        <div className={styles.inputGroup}>
-          <label htmlFor="grade" className="label">
-            Grade:
+        {policy.map((pol, i) => (
+        <div key={i} className={styles.policy_container}>
+          <div className={styles.input_contain}>
+            <label>Policy Name</label>
+            <input
+              type="input"
+              placeholder="Policy Name"
+              className={styles.input_box}
+              value={pol.name}
+              onChange={(e) => handleChange(i, "name", e.target.value)}
+            />
+          </div>
+          <div className={styles.input_contain}>
+            <label>Weightage</label>
+            <input
+              type="number"
+              placeholder="Weightage (0% to 100%)"
+              className={styles.input_box}
+              value={pol.weightage === 0 ? "" : pol.weightage}
+              onChange={(e) =>
+                handleChange(i, "weightage", Number(e.target.value))
+              }
+            />
+          </div>
+          <div className={styles.input_contain}>
+            <label>Grade</label>
+            <input
+              type="decimal"
+              placeholder="Grade (0 to 100)"
+              className={styles.input_box}
+              value={pol.grade === 0 ? "" : pol.grade}
+              onChange={(e) => handleChange(i, "grade", Number(e.target.value))}
+            />
+          </div>
+        </div>
+      ))}
+        
+        {schedule.map((s,i)=>(
+          <div key={i} className={styles.policy_container}>
+<div className={styles.inputGroup}>
+          <label htmlFor="day" className="label">
+            Day:
           </label>
           <input
-            id="grade"
+            id="day"
             className="input"
-            type="number"
-            step="0.01"
-            value={grade}
-            onChange={(event) => setGrade(parseFloat(event.target.value))}
+            type="text"
+            value={s.day}
+            onChange={(event) => handleSchedChange(i, "day", event.target.value)}
           />
         </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="time" className="label">
+            Time Start:
+          </label>
+          <input
+            id="time"
+            className="input"
+            type="text"
+            value={s.startTime}
+            onChange={(event) => handleSchedChange(i, "startTime", event.target.value)}
+          />
+        </div>
+        <div className={styles.inputGroup}>
+          <label htmlFor="time" className="label">
+            Time Finish:
+          </label>
+          <input
+            id="time"
+            className="input"
+            type="text"
+            value={s.endTime}
+            onChange={(event) => handleSchedChange(i, "endTime", event.target.value)}
+          />
+        </div>
+          </div>
+        ))}
+        
+        
         <button className={styles.submitBtn} onClick={submit}>
           Submit Grade
         </button>
+        <button className={styles.submitBtn} onClick={newSched} disabled={schedule.length >= 5}>
+          Add Time
+        </button>
+        <button className={styles.submitBtn} onClick={removeSched} disabled={schedule.length <= 1}>
+          Remove Time
+        </button>
+        <button className={styles.submitBtn} onClick={newPolicy}
+          disabled={policy.length >= 5}>
+          Add Policy
+        </button>
+        <button className={styles.submitBtn} onClick={removePolicy}
+          disabled={policy.length <= 1}>
+          Remove Policy
+        </button>
+
+
       </div>
     </div>
   );
